75634e4fbd06e03c3e6e846986c16cb924e9495f
Kevin Chapman <kchapmann@gmail.com>
2014-04-11 16:56:31 -0400

    adding main, scene, some stuff in model, and a .gitignore.

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+
 /**
  * Created by kevin on 4/11/14.
  */
 public class Model {
+    private static LinkedList<Scene> timeline = new LinkedList<Scene>();
+
+    public Scene getScene(int i){
+        if (i > timeline.size()){
+            throw new ArrayIndexOutOfBoundsException("that index has no corresponding scene.");
+        }
+
+        else{
+            return timeline.get(i);
+        }
+    }
+
+    private void newScene(Scene s){
+        if (timeline.contains(s)){
+            throw new InputMismatchException("that scene already exists");
+        }
+
+        else{
+            timeline.add(s);
+        }
+    }
+
 }
---
+++ src/Scene.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kevin on 4/11/14.
+ */
+public class Scene {
+}
